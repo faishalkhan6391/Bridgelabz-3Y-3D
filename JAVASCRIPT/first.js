cb52a189b478fc5be7b382e5f644ac394510dfb0
faishalkhan6391 <faishalkhan0410@gmail.com>
2025-11-10 12:00:42 +0530

    Added new content to webdevlopment branch

---
+++ JAVASCRIPT/first.js
@@ -0,0 +1,31 @@
+console.log("gcbsb");
+let a=10;
+console.log(a)
+let b;
+console.log(b)
+let c=Symbol('id');
+console.log(c)
+
+const func = ()=>{
+    console.log("Hdlloo")
+}
+func();
+
+let arr =[1,2,3,3];
+console.log(arr[1]);
+
+// OBJECT
+
+let num={
+    Name:"faishal",
+    Age:20,
+}
+console.log(num["Name"])
+console.log(num["Age"])
+console.log(num.Age)
+
+let num1=20;
+let num2=30;
+console.log(num1+num2);
+console.log(num1/num2);
+
