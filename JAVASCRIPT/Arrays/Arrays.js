49bb08804be5c26fb55828f41412850266762cae
faishalkhan6391 <faishalkhan0410@gmail.com>
2025-11-13 16:03:09 +0530

    Add Array  Folder

---
+++ JAVASCRIPT/Arrays/Arrays.js
@@ -0,0 +1,10 @@
+let arr =[1,2,3,4,5]
+// let mapArr=arr.map((x)=> x*2);
+// arr.filter((x)=> x%2==0)
+
+// arr.reduce((a,b)=>a+b)
+
+// arr.shift(); //Remove first Element
+// console.log(arr)
+// arr.unshift(7); //Add at starting
+// console.log(arr)
