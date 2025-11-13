f7122eae09bc5180d102b98115e235ff690ea977
faishalkhan6391 <faishalkhan0410@gmail.com>
2025-11-13 16:02:06 +0530

    Add String  Folder

---
+++ JAVASCRIPT/String/String.js
@@ -0,0 +1,24 @@
+// String is premitive data type
+let a="Hii";
+let b='Hii'
+let c=`Hii`
+// concate
+let d=a+" "+b  //1st way
+let e=a.concat(b);//2nd way
+let f=`Hii${b}`//3rd way
+console.log(f);
+
+// substring
+let str="hello";
+console.log(str.substring(1,3));
+
+// lastIndexof
+"Hello".lastIndexOf("1")
+
+
+// slice
+"Hello".slice(-1,4)
+
+// split
+"Hello".split(' ');
+
