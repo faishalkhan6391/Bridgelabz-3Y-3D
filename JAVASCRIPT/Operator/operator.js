006c68f2e0fb2b5995f62ac873d1dd200791357e
faishalkhan6391 <faishalkhan0410@gmail.com>
2025-11-13 15:58:40 +0530

    Add Operator  Folder

---
+++ JAVASCRIPT/Operator/operator.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = 3;
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+let remainder = a % b;
+
+let isEqual = a == b;
+let isNotEqual = a != b;
+let isGreater = a > b;
+let isLess = a < b;
+
+let andOperator = (a > 5 && b < 5);
+let orOperator = (a > 5 || b > 5);
+let notOperator = !(a > b);
+
+a += 2;
+b *= 3;
+
+let result = (a > b) ? a
