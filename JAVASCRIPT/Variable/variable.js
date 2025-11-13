42748d08986580bda44c639cd753f29ef19db4ae
faishalkhan6391 <faishalkhan0410@gmail.com>
2025-11-13 14:20:49 +0530

    Add Variable Folder

---
+++ JAVASCRIPT/Variable/variable.js
@@ -0,0 +1,37 @@
+console.log("gcbsb");
+let v=10;
+console.log(a)
+let z;
+console.log(b)
+let c=Symbol('id');
+console.log(c)
+
+let name = "Alice";
+let age = 25;
+const country = "India";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Country:", country);
+
+
+let score = 50;
+console.log("Initial score:", score);
+
+score = 75; // update value
+console.log("Updated score:", score);
+
+
+const pi = 3.1416;
+// pi = 3.14; ❌ Error — cannot change const value
+console.log(pi);
+
+
+let a = 10;
+let b = 5;
+
+let sum = a + b;
+let product = a * b;
+
+console.log("Sum:", sum);
+console.log("Product:", product);
